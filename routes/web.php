da76f41f233ce49aacfb1f905e2c70bfa715a45d
faghihi <h.faghihi15@gmail.com>
2017-04-12 13:36:23 +0430

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/set', function () {
 
 });
 
-Route::post('/343139142:AAGCpIOGVwFcbbJL56sLVSXUgz8zO3jPc34/webhook','TelegramController@run');
+Route::post('/376682828:AAE79WT571kMAmUk77iD3KJIni0ml7xixEs/webhook','TelegramController@run');
 
 Route::get('testdatabase',function (){
     return \App\Conversation::all();
