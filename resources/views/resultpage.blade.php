a911994fc14ade50beeb5d114600aa6ad96145fe
hossein <h.faghihi15@gmail.com>
2017-04-19 17:53:16 +0430

    table fixed

--- resources/views/resultpage.blade.php
+++ resources/views/resultpage.blade.php
@@ -3,11 +3,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Laravel</title>
+    <link href="/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-<table>
+<table class="table table-responsive">
     <tr>
         <th>
             name
@@ -45,5 +45,6 @@
         </tr>
     @endforeach
 </table>
+<script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
