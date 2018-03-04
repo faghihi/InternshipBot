0e429873780b0e1dfe42efa9be4c19c271ceb32c
hossein <h.faghihi15@gmail.com>
2018-03-04 21:24:28 +0330

    expection added

--- app/Http/Controllers/telegramtestcontroller.php
+++ app/Http/Controllers/telegramtestcontroller.php
@@ -11,11 +11,17 @@ class telegramtestcontroller extends Controller
     public function index()
     {
         foreach(Conversation::all() as $conversation){
-            \Telegram::sendMessage(
-                [
-                    'chat_id' => $conversation->chat_id,
-                    'text' => 'بعد از رزو زمان مصاحبه توسط بات نیاز به انتظار برای تماس از سمت ما نیست . لطفا در زمان مقرر شده توسط بات در شرکت حضور داشته باشید :-)',
-                ]);
+            try{
+                \Telegram::sendMessage(
+                    [
+                        'chat_id' => $conversation->chat_id,
+                        'text' => 'بعد از رزو زمان مصاحبه توسط بات نیاز به انتظار برای تماس از سمت ما نیست . لطفا در زمان مقرر شده توسط بات در شرکت حضور داشته باشید :-)',
+                    ]);
+            }
+            catch (\Exception $e){
+                continue;
+            }
+
         }
     }
 }
